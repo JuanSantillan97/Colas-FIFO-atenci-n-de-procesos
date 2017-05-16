5449e11accdef775b04cedb3c0b9452cd48d4482
JuanSantillan97 <jsantillan@ucol.mx>
2017-05-16 16:07:34 -0500

    Primera versión.
    Todo arreglado, espero xd.

---
+++ Procesador.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_FIFO_Atencion_de_procesos
+{
+    class Procesador
+    {
+        private Proceso inicio;
+
+        public Procesador()
+        {
+            inicio = null;
+        }
+        
+        //Agregar (PUSH).
+        public void agregarPush(Proceso nuevoPro)
+        {
+            if(inicio == null)
+            {
+                inicio = nuevoPro;
+            }
+            else
+            {
+                Proceso temp = inicio;
+                while (temp.siguiente != null)
+                {
+                    temp = temp.siguiente;
+                }
+                temp.siguiente = nuevoPro;
+            }
+        }
+        //Eliminar (POP).
+        public Proceso eliminarPop()
+        {
+            Proceso temp = inicio;
+            if(inicio == null)
+            {
+                return null;
+            }
+            else
+            {
+                inicio = inicio.siguiente;
+                return temp;
+            }
+        }
+        //Método para regresar el total de ciclos 
+        //de los procesos en la cola.
+        public int regresarTotalCiclos()
+        {
+            Proceso temp = inicio;
+            int totalCiclos = 0;
+            while(temp.siguiente != null)
+            {
+                totalCiclos += temp.ciclos;
+                temp = temp.siguiente;
+            }
+            totalCiclos += temp.ciclos;
+            return totalCiclos;
+        }
+        //Método que regresa el inicio o null.
+        public Proceso regresarInicio()
+        {
+            return inicio;
+        }
+
+        public override string ToString()
+        {
+            Proceso temp = inicio;
+            string cadena = "";
+            while (temp.siguiente != null)
+            {
+                cadena += temp.ToString();
+                temp = temp.siguiente;
+            }
+            cadena += temp.ToString();
+            return cadena;
+        }
+    }
+}
---
+++ Proceso.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas_FIFO_Atencion_de_procesos
+{
+    class Proceso
+    {
+        public Proceso siguiente;
+        static Random numAle = new Random();
+
+        private int _ciclos;
+        public int ciclos
+        {
+            get { return _ciclos; }
+        }
+
+        public Proceso()
+        {
+            _ciclos = numAle.Next(4, 14);
+        }
+
+        public void disminuir()
+        {
+            _ciclos--;
+        }
+
+        public override string ToString()
+        {
+            return "Ciclos: " + _ciclos + "\r\n";
+        }
+
+    }
+}
